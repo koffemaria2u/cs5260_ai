3ffbdcf2e3622a07d38412b1a744d04e9c6984f1
koffemaria2u <koffemaria@2u.com>
2023-04-22 16:14:06 -0400

    part2: implement feats 1-5, fixes 1-5

--- src/world_search.py
+++ src/world_search.py
@@ -3,20 +3,25 @@ import numpy as np
 import datetime
 import random
 from queue import PriorityQueue
-from util import find_schedule_info, write_file, stringify_schedule
+from util import find_schedule_info, write_file, stringify_schedule, plot_results, write_summary_file
 from base_classes import TransferTemplate, Node, TransformTemplate
 
 # constant values for expected utility
-
-# Test Case 1
 GAMMA = 0.95
-COST_OF_FAILURE = -0.10
+COST_OF_FAILURE = -10
+L = 1
+K = 0.001
+X_0 = 5
+
+"""
+# Test Case 1, Part 1
+GAMMA = 0.95
+COST_OF_FAILURE = -5
 L = 1
 K = 1
 X_0 = 0
-"""
 
-# Test Case 2
+# Test Case 2, Part 1
 GAMMA = 0.95
 COST_OF_FAILURE = -10
 L = 1
@@ -29,42 +34,55 @@ CURRENT_DATETIME = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 class WorldSearch:
     def __init__(self, root_node, max_depth, resource_list, your_country_name, output_schedule_filename,
-                 num_output_schedules, frontier_max_size, logger):
+                 num_output_schedules, frontier_max_size, logger, search_type):
         self.root_node = root_node              # root/parent Node to start
         self.max_depth = max_depth              # max depth to go down the search tree
         self.resource_list = resource_list      # list of initial resources
         self.iam_country = your_country_name    # name of country the agent plays
         self.output_schedule_filename = output_schedule_filename + "_" + str(max_depth) + "depth_" + CURRENT_DATETIME + ".txt"
+        self.output_schedule_plotfile = f"src/output_files/result_plots/{search_type}_{your_country_name}_" + str(max_depth) + "depth_" + CURRENT_DATETIME + ".png"
         self.frontier_max_size = frontier_max_size          # max amount of nodes on the frontier
         self.num_output_schedules = num_output_schedules    # max number of schedules
         self.logger = logger                    # logger
+        self.search_type = search_type
         self.frontier = self.init_frontier()    # sets up the frontier
         self.best_node_eu = (0, root_node)  # to start, the best node is the root/parent node
+        self.best_schedule_list = []
         self.final_schedule = "["    # empty schedule string, to be populated with schedules with the best EU scores
         self.schedule_count = 1     # keeps track of the count of schedules
         self.current_depth = 1      # keeps track of the current depth of search tree
         self.successor_child_count = 0    # keeps track of the count of successor child nodes
         self.node_count_id = root_node.node_id  # keeps track of node count and use as an ID; includes root node
-        self.world_population = self.compute_world_population("Population")
-        self.world_electronics = self.compute_world_population("Electronics")
+        self.world_population = self.compute_world_resource("Population")   # currently unused
+        self.world_electronics = self.compute_world_resource("Electronics") # currently unused
+        self.recycled_electronics = 0
 
-    def compute_world_population(self, reource_type):
+    def compute_world_resource(self, resource_type):
         """
         Compute world population based on initial world state
+            CURRENTLY NOT USED
         """
         world_resource = 0
         for country in self.root_node.world_state.country_list:
-            world_resource += country[reource_type]
+            world_resource += country[resource_type]
 
         return world_resource
 
     def init_frontier(self):
         """
-        Helper function to setup the frontier using PriorityQueue
+        Helper function to setup the frontier depending on the search type and append init root node
+            HDFS = Python built-in List
+            GBFS = PriorityQueue
         """
-        self.logger.info(f"Initializing frontier using with the root node...")
-        frontier = PriorityQueue()
-        frontier.put((0, self.root_node))
+        self.logger.info(f"Initializing frontier for search type {self.search_type}...")
+        if self.search_type in ["hdfs", "bfs"]:
+            frontier = [(0, self.root_node)]
+        elif self.search_type == "gbfs":
+            frontier = PriorityQueue()
+            frontier.put((0, self.root_node))
+        else:
+            raise ValueError("Invalid selection search_type=%s" % self.search_type)
+
         return frontier
 
     def validate_iam_country(self, country):
@@ -77,9 +95,78 @@ class WorldSearch:
         else:
             return False
 
-    def bfs_search(self):
+    def hdfs_search(self):
         """
-        Breadth-First Search algorithm as taken from sample code BreadthFirstSearch.py
+        NEW FEAT 5
+        Heuristic Depth-First Search algorithm w/ reached.
+        To prevent searching down nodes with bad EU scores, the agent prunes the lower scored nodes and only takes
+        the top 2 nodes (shuffled) to append on the frontier.
+            source: https://www.youtube.com/watch?v=pcKY4hjDrxk&t=730s
+        """
+        visited = set()
+        while self.successor_child_count <= self.frontier_max_size or self.current_depth > self.max_depth:
+            # on the latest set of appended nodes, pop the node with the highest EU score
+            eu_score, current_node = self.frontier.pop()
+            # print(f"{eu_score}, node_id {current_node.node_id}")
+
+            # helps limit the depth of schedules to the max depth arg
+            sched_count_depth = find_schedule_info(current_node, find_successor_count=True)
+            if sched_count_depth >= self.max_depth:
+                continue
+
+            # check the current score against the best score found so far
+            if current_node.iam_country == self.iam_country and current_node not in visited:
+                self.evaluate_best_score(eu_score=eu_score, current_node=current_node)
+            else:
+                continue
+
+            visited.add(current_node)
+
+            # if there's no child nodes, generate successors of your current node
+            child_node_successors = self.create_child_nodes(parent_node=current_node)
+            self.successor_child_count += len(child_node_successors)
+
+            # evaluate EU scores all children nodes generated
+            temp_child_node_successors = []
+            for child_node in child_node_successors:
+                if child_node is None:
+                    continue
+
+                # calculate EU score; insert child into current node
+                eu_score = self.expected_utility(child_node)
+                current_node.insert_child_node(child_node)
+
+                # append score and node to temp list
+                temp_child_node_successors.append((eu_score, child_node))
+
+                # update the current depth
+                child_node_count = find_schedule_info(child_node, find_successor_count=True)
+                self.current_depth = max(self.current_depth, child_node_count)
+
+            # Prune the lowest scores
+            # sort highest to lowest; take best 2 scores; shuffle and insert to frontier
+            # temp_child_node_successors.sort(reverse=True)
+            # best_two = temp_child_node_successors[:2]
+            # random.shuffle(best_two)
+            # self.frontier.extend(best_two)
+
+            random.shuffle(temp_child_node_successors)
+            self.frontier.extend(temp_child_node_successors)
+
+        plot_results(output=self.best_schedule_list, output_file=self.output_schedule_plotfile)
+        write_summary_file(output_file=self.output_schedule_filename, iam_country=self.iam_country,
+                           best_eu_score=self.best_node_eu[0], schedule_count=self.schedule_count,
+                           node_count=self.node_count_id)
+        self.logger.info(f"HDFS complete! Check resuls in file: {self.output_schedule_filename}")
+        return self.best_node_eu, self.current_depth, self.node_count_id, self.schedule_count
+
+    def bfs_search(self):
+        pass
+
+    def gbfs_search(self):
+        """
+        Greedy Best First Search algorithm
+            source: https://www.youtube.com/watch?v=dv1m3L6QXWs
         """
         # project req: end the search when the node count exceeds a max frontier size value or max depth of search space
         while self.successor_child_count <= self.frontier_max_size or self.current_depth > self.max_depth:
@@ -96,11 +183,16 @@ class WorldSearch:
                 continue
 
             # check the current score against the best score found so far
-            self.evaluate_best_score(eu_score=eu_score, current_node=current_node)
+            # FIXED 1, see notes #1
+            if current_node.iam_country == self.iam_country:
+                self.evaluate_best_score(eu_score=eu_score, current_node=current_node)
 
             # start generating successors of your current node from the frontier
             child_node_successors = self.create_child_nodes(parent_node=current_node)
             self.successor_child_count += len(child_node_successors)
+
+            # FIXED 2: randomize templates, see notes #2
+            random.shuffle(child_node_successors)
 
             # evaluate EU scores all children nodes generated
             for child_node in child_node_successors:
@@ -114,11 +206,14 @@ class WorldSearch:
                 current_node.insert_child_node(child_node)
 
                 # update the current depth if needed
-                # child_node_count = get_schedule_count_depth(child)
                 child_node_count = find_schedule_info(child_node, find_successor_count=True)
                 self.current_depth = max(self.current_depth, child_node_count)
 
-        self.logger.info(f"BFS complete! Check resuls in file: {self.output_schedule_filename}")
+        self.logger.info(f"GBFS complete! Check resuls in file: {self.output_schedule_filename}")
+        plot_results(output=self.best_schedule_list, output_file=self.output_schedule_plotfile)
+        write_summary_file(output_file=self.output_schedule_filename, iam_country=self.iam_country,
+                           best_eu_score=self.best_node_eu[0], schedule_count=self.schedule_count,
+                           node_count=self.node_count_id)
         return self.best_node_eu, self.current_depth, self.node_count_id, self.schedule_count
 
     def evaluate_best_score(self, eu_score, current_node):
@@ -129,6 +224,7 @@ class WorldSearch:
         if eu_score > self.best_node_eu[0]:
             self.logger.info(f"found higher score: {eu_score}")
             self.best_node_eu = (eu_score, current_node)
+            self.best_schedule_list.append((eu_score, current_node))
             if self.schedule_count <= self.num_output_schedules:
                 best_schedule = stringify_schedule(self.best_node_eu[1], self.schedule_count, self.node_count_id, self.current_depth, eu_score)
                 write_file(self.output_schedule_filename, best_schedule)
@@ -152,30 +248,33 @@ class WorldSearch:
         # loop through each item in transform_templates, and each country in country_list
         # set possible actions according to each template
         for template in transform_templates:
-            for i in range(len(country_list)):
+            # create a set of variables to denote the current world state, country list, transform actions
+            world_country_clone_transform = world_state_parent.copy_world_state()
+            # FIXED 1, see notes #1; we only care about the self country, in this case NCR
+            # current_country_transform = world_country_clone_transform.country_list[0] # this works
+            current_country_transform = world_country_clone_transform.get_country(self.iam_country)
+            action_transform = TransformTemplate(current_country_transform, template)
 
-                # create a set of variables to denote the current world state, country list, transform actions
-                world_country_clone_transform = world_state_parent.copy_world_state()
-                current_country_transform = world_country_clone_transform.country_list[i]
-                action_transform = TransformTemplate(current_country_transform, template)
+            # skip is there is no transform action available
+            if not action_transform:
+                continue
 
-                # skip is there is no transform action available
-                if not action_transform:
-                    continue
+            # create and append child node to the return list var
+            # includes transform template action
+            self.node_count_id += 1
+            child_node = Node(parent_node=parent_node,
+                              world_state=world_country_clone_transform,
+                              action=action_transform.transform_template,
+                              your_country_name=self.iam_country,
+                              node_count_id=self.node_count_id,
+                              frontier_max_size=self.frontier_max_size)
 
-                # create and append child node to the return list var
-                # includes transform template action
-                self.node_count_id += 1
-                child_node = Node(parent_node=parent_node,
-                                  world_state=world_country_clone_transform,
-                                  action=action_transform.transform_template,
-                                  your_country_name=self.iam_country,
-                                  node_count_id=self.node_count_id
-                                  )
+            child_nodes.append(child_node)
 
-                child_nodes.append(child_node)
-
-        transfer_resources = ["MetallicElements", "Timber", "MetallicAlloys", "Electronics"]
+        # TODO 4/17 temp remove electronics from trading
+        transfer_resources = ["MetallicElements", "Timber", "MetallicAlloys",
+                              # "Electronics"
+                              ]
 
         # set a random percentage between 10% to 30% for transferring resources
         transfer_percentage = random.uniform(0.1, 0.3)
@@ -213,7 +312,8 @@ class WorldSearch:
                                   world_state=world_country_clone_transfer,
                                   action=action_transfer.write_transfer_template(),
                                   your_country_name=self.iam_country,
-                                  node_count_id=self.node_count_id
+                                  node_count_id=self.node_count_id,
+                                  frontier_max_size=self.frontier_max_size
                                   )
                 child_nodes.append(child_node)
 
@@ -300,20 +400,63 @@ class WorldSearch:
 
     def state_quality(self, node):
         """
+        NEW FEAT 3
         State Quality goal is to produce maximum electronics given the available resources
             Measure how much electronics you currently have
             Measure how much electronics you can potentially create, given your current amount of resources available
+            If producing more Electronics against country population, penalize Electronics, MetallicElements, MetallicAlloys
+            if producing too much Housing against country population, penalize Housing
         """
         result_score = 0
 
         # State Quality is always counted against the "self" country
         iam_country = node.invoke_get_country()
+        e_per_person = 2
+        electronics_population_ratio = math.ceil(iam_country["Population"] * e_per_person)
+        h_threshold = 1.25  # set to 1 for now
+        h_per_person = 4
         for resource_name, resource_amount in iam_country.items():
+            # reset this everytime
+            e_threshold = 1.5
             # exclude from processing Country as a resource
             if resource_name == "Country":
                 continue
 
-            if resource_name == "Electronics":
+            elif resource_name == "Housing":
+                resource_weight = self.resource_list.get(resource_name)["weight"]
+
+                # NEW FEAT 3: improve SQ complexity
+                # if producing too much electronics against world population, penalize
+                # assume that each person can handle/own 2 electronics at a time
+                housing_population_ratio = math.ceil(iam_country["Population"] / h_per_person)
+                if resource_amount > housing_population_ratio:
+                    # apply Electronics threshold to discourage acquiring more electronics
+                    h_threshold = 0.33
+
+                h_sub_score = resource_weight * resource_amount * h_threshold
+                result_score += h_sub_score
+
+            elif resource_name == "MetallicElements":
+                resource_weight = self.resource_list.get(resource_name)["weight"]
+
+                if resource_amount > electronics_population_ratio:
+                    # apply Electronics threshold to discourage acquiring more electronics
+                    e_threshold = 0.33
+
+                me_sub_score = resource_weight * resource_amount * e_threshold
+                result_score += me_sub_score
+
+            elif resource_name == "MetallicAlloys":
+                resource_weight = self.resource_list.get(resource_name)["weight"]
+
+                if resource_amount > electronics_population_ratio:
+                    # apply Electronics threshold to discourage acquiring more electronics
+                    e_threshold = 0.33
+
+                ma_sub_score = resource_weight * resource_amount * e_threshold
+                result_score += ma_sub_score
+
+            elif resource_name == "Electronics":
                 resource_weight = self.resource_list.get(resource_name)["weight"]
                 resource_metals = iam_country["MetallicElements"]
                 resource_alloys = iam_country["MetallicAlloys"]
@@ -326,16 +469,17 @@ class WorldSearch:
                 discount_unbuilt_electronics = 0.5
                 unbuilt_electronics_score = raw_materials_to_produce_electronics * discount_unbuilt_electronics
 
+                # NEW FEAT 3: improve SQ complexity
                 # if producing too much electronics against world population, penalize
                 # assume that each person can handle/own 2 electronics at a time
-                # TODO WIP - I did not have enough time to properly implement this; set to 1 for now
-                e_threshold = 1     # set to 1 for now
-                # if self.world_electronics > (self.world_population * 1.5):
-                #     e_threshold = 0.5
+                if resource_amount > electronics_population_ratio:
+                    # apply Electronics threshold to discourage acquiring more electronics
+                    e_threshold = 0.33
+                    unbuilt_electronics_score = 0
 
                 e_sub_score = resource_weight * resource_amount * e_threshold
-
-                result_score += e_sub_score + unbuilt_electronics_score
+                pre_result = e_sub_score + unbuilt_electronics_score
+                result_score += pre_result
 
             elif resource_name == "ElectronicsWaste":
                 resource_weight = self.resource_list.get(resource_name)["weight"]
